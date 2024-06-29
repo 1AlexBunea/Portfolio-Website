19ffde9f936a082720d24b34c386dd06f1760f79
1AlexBunea <1alexbunea@gmail.com>
2024-06-28 17:36:10 -0700

    sidebar changes

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -9,8 +9,8 @@ const Sidebar: React.FC = () => {
         marginTop:20,
     }}>
       <ul>
-        <li><a href="#section1">Section 1</a></li>
-        <li><a href="#section2">Section 2</a></li>
+        <li><a href="#section1">Github</a></li>
+        <li><a href="#section2">LinkedIn</a></li>
         <li><a href="#section3">Section 3</a></li>
       </ul>
     </div>
