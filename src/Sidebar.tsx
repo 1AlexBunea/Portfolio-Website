8f983e0f7fe9f7181755815278993d63c30ebc5a
1AlexBunea <1alexbunea@gmail.com>
2024-06-28 18:07:47 -0700

    sidebar changes

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -8,11 +8,15 @@ const Sidebar: React.FC = () => {
     <div className="sidebar" style={{
         marginTop:20,
     }}>
-      <ul>
-        <li><a href="#section1">Github</a></li>
-        <li><a href="#section2">LinkedIn</a></li>
-        <li><a href="#section3">Section 3</a></li>
-      </ul>
+      <div className='rotated-items'>
+        <div className='rotated-text'>
+          <h1>___</h1>
+        </div>
+        <button className='btn btn-outline-dark'>1alexbunea@gmail.com</button>
+        <div className='rotated-text'>
+          <h1>__________________________________________________</h1>
+        </div>
+      </div>
     </div>
   );
 };
